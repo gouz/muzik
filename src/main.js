4791353f6583b764ef3b56395651581e84162cc5
Sylvain Gougouzian <sylvain@lidrea.fr>
2021-10-21 23:48:10 +0200

    refactor: :zap: No lazy, random onload

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import "./main.css";
 import "./less/main.less";
-import LazyLoad from "vanilla-lazyload";
 
 const template = (url) => {
     if (url.indexOf('youtube') !== -1 || url.indexOf('youtu.be') !== -1) {
@@ -9,8 +8,7 @@ const template = (url) => {
         <div class="🎶">
             <div class="🎶--box">
                 <iframe 
-                    class="lazy" 
-                    data-src="https://www.youtube.com/embed/${code}" 
+                    src="https://www.youtube.com/embed/${code}" 
                     allowFullScreen="allowFullScreen" 
                     width="640" height="360"
                     allowtransparency="true" 
@@ -18,39 +16,15 @@ const template = (url) => {
                     allow="autoplay"
                 >
                 </iframe>
-                <div class="lazy" data-lazy-function="loadMoreSongs">...</div>
+                <a href="#" onclick="window.location.reload(); return false;">next</div>
             </div>
         </div>
         `;
     }
     return '';
-}
-
-const $wrapper = document.querySelector("#wrapper");
-
-const executeLazyFunction = (element) => {
-    let lazyFunctionName = element.getAttribute("data-lazy-function");
-    let lazyFunction = window.lazyFunctions[lazyFunctionName];
-    if (!lazyFunction) return;
-    lazyFunction(element);
-}
-const lazyLoadOptions = {
-    unobserve_entered: true, // <- Avoid executing the function multiple times
-    callback_enter: executeLazyFunction // Assigning the function defined above
 };
-const pageLazyLoad = new LazyLoad(lazyLoadOptions);
 
-window.songList = [];
-
-const loadSongs = () => {
-    if (window.songList.length !== 0) {
-        let url = window.songList.shift();
-        $wrapper.innerHTML += template(url);
-    }
-    pageLazyLoad.update();
-}
-
-function shuffle(array) {
+const shuffle = (array) => {
     let currentIndex = array.length, randomIndex;
     while (currentIndex != 0) {
         randomIndex = Math.floor(Math.random() * currentIndex);
@@ -59,18 +33,13 @@ function shuffle(array) {
     }
 
     return array;
-}
+};
 
 fetch("./list.json").then((response) => {
     return response.json().then((json) => {
-        window.songList = shuffle(json.list);
-        loadSongs();
+        let list = shuffle(json.list);
+        let url = list.shift();
+        document.querySelector("#wrapper").innerHTML += template(url);
     });
 });
 
-window.lazyFunctions = {
-    loadMoreSongs: function (element) {
-        element.parentNode.removeChild(element);
-        loadSongs();
-    }
-}

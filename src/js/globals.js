a35d5d8443d3c732728a1dd67c0c1592856624d7
Sylvain Gougouzian <sylvain@lidrea.fr>
2022-01-21 08:47:09 +0100

    feat: :lipstick: video button position

--- src/js/globals.js
+++ src/js/globals.js
@@ -122,6 +122,7 @@ window.muzik.next = (force) => {
 window.muzik.showClip = () => {
   if ("youtube" == window.muzik.song.type) {
     document.getElementById("youtube-player").classList.toggle("over");
+    window.muzik.$video.classList.toggle("active");
   }
 };
 
